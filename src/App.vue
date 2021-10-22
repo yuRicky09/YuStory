93d2deb91f4d9639bd84ee09acc2f12272858ded
Yu Ricky <leoleo5994@gmail.com>
2021-10-22 16:28:52 +0800

    YUSTORY project init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <p>test english</p>
+    <h1>test title</h1>
+    <p>測試中文字</p>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import "../node_modules/modern-normalize/modern-normalize.css";
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300;400;500;700&family=Quicksand:wght@300;400;500;600;700&display=swap");
+
+:root {
+  --color-gray-1: #292929;
+  --color-gray-2: #757575;
+  --color-bg-dark-1: #191919;
+  --color-bg-dark-2: #303030;
+  --color-bg-white-1: #f2f2f2;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+* {
+  font-family: "Quicksand", "Noto Sans TC", sans-serif;
+  margin: 0;
+  padding: 0;
+  line-height: 1.5;
 }
 </style>
---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: `${process.env.VUE_APP_FIREBASE}`,
+  authDomain: "yu-story.firebaseapp.com",
+  projectId: "yu-story",
+  storageBucket: "yu-story.appspot.com",
+  messagingSenderId: "362985153097",
+  appId: "1:362985153097:web:68d3fe72a5603f2437d598",
+  measurementId: "G-2PCR4WS2PF",
+};
